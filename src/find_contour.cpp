21e33f25903f714daf9bf4f866229662d9360a7b
Jister <chenjiesjtu14@gmail.com>
2016-05-27 22:55:17 +0800

    add maxarea fit

--- src/find_contour.cpp
+++ src/find_contour.cpp
@@ -190,14 +190,19 @@ void FindContour::altitudeCallback(const ardrone_autonomy::navdata_altitude &msg
 {
 	if(msg.altitude_vision/1000.0 < 1.5){
 		minarea = 50000;
+		maxarea = 200000;
 	}else if(msg.altitude_vision/1000.0 < 2.0){
 		minarea = 30000;
+		maxarea = 180000;
 	}else if(msg.altitude_vision/1000.0 < 2.5){
 		minarea = 20000;
+		maxarea = 160000;
 	}else if(msg.altitude_vision/1000.0 < 3.0){
 		minarea = 16000;
+		maxarea = 100000;
 	}else{
 		minarea = 12000;
+		maxarea = 50000;
 	}
 }
 
